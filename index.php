8b6ef19ccc4701149a53a77e41c686f32b77117a
Dillon Duncan <dillonduncan0607@gmail.com>
2024-06-03 21:24:31 -0500

    arrays

--- index.php
+++ index.php
@@ -1,54 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Fundamentos de php</h1>
     <?php
-        #imprimir
-        echo"<p>Habla parce</p>";
-        print("habla burro");
-        /*comentarios*/
-        //comentario
+    #imprimir
+    echo "<p>Habla parce</p>";
+    print("habla burro");
+    /*comentarios*/
+    //comentario
 
-        echo "<!--comentario html-->";
+    echo "<!--comentario html-->";
 
-        #Variables
-        //el punto(.) se usa para concatenar
-        // if (isset($_GET["name"])) {
-        //     $nombre=$_GET["name"];
-        // }else{
-        //     $nombre= "Dillon";
-        // }
-        $nombre= validarName("name");
-        $edad=18;
-        echo "<p>"."Nombre: ".$nombre." Edad:".$edad. "</p>";
+    #Variables
+    //el punto(.) se usa para concatenar
+    // if (isset($_GET["name"])) {
+    //     $nombre=$_GET["name"];
+    // }else{
+    //     $nombre= "Dillon";
+    // }
+    $nombre = validarName("name");
+    $edad = 18;
+    echo "<p>" . "Nombre: " . $nombre . " Edad:" . $edad . "</p>";
 
-        #GET
-        echo"<hr/>";
-        // $_GET["name"];
+    #GET
+    echo "<hr/>";
+    // $_GET["name"];
 
-        #funciones
-        // function validarName(){
-        //     if (isset($_GET["name"])) {
-        //         $nombre=$_GET["name"];
-        //     }else{
-        //         $nombre= "Dillon";
-        //     }
-        //     return $nombre;
-        // }
- function validarName($valor){
-            if (isset($_GET["$valor"])) {
-                $val=$_GET["$valor"];
-            }else{
-                $val= "por defecto";
+    #funciones
+    // function validarName(){
+    //     if (isset($_GET["name"])) {
+    //         $nombre=$_GET["name"];
+    //     }else{
+    //         $nombre= "Dillon";
+    //     }
+    //     return $nombre;
+    // }
+    function validarName($valor)
+    {
+        if (isset($_GET["$valor"])) {
+            $val = $_GET["$valor"];
+        } else {
+            $val = "por defecto";
+        }
+        return $val;
+    }
+
+    #Arrays
+    $personas=["Junior","Dillon","Naisir"];
+    echo $personas[0];
+    ?>
+
+    <h3>Listado Array</h3>
+    <ul>
+        <?php
+            foreach ($personas as $person) {
+                echo "<li>". $person ."</li>";
             }
-            return $val;
-        }    
-        
         ?>
+    </ul>
 </body>
+
 </html>
