4cf513ff7c312e4df5ea935249164ca8439dc65e
Dillon Duncan <dillonduncan0607@gmail.com>
2024-06-03 20:24:38 -0500

    primeros fundamentos

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Fundamentos de php</h1>
+    <?php
+        #imprimir
+        echo"<p>Habla parce</p>";
+        print("habla burro");
+        /*comentarios*/
+        //comentario
+
+        echo "<!--comentario html-->";
+
+        #Variables
+        //el punto(.) se usa para concatenar
+        $nombre="Dillon";
+        $edad=18;
+        echo "<p>"."Nombre: ".$nombre." Edad:".$edad. "</p>";
+    ?>
+</body>
+</html>
